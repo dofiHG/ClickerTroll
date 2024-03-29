43da88d8e8045bc4956798787fc19260b9fbf403
dofiHG <dofihgsafafafa@gmail.com>
2024-03-29 20:00:52 +0300

    AddAnimationAndConter

--- Clicker/Assets/Scripts/TrollCounter.cs
+++ Clicker/Assets/Scripts/TrollCounter.cs
@@ -2,8 +2,11 @@ using UnityEngine;
 
 public class TrollCounter : MonoBehaviour
 {
-    public int trollPerClick;
-    public int trollPerSecond;
-    public int countTrollNow;
-    public int levelProgress;
+    public int trollPerClick = 1;
+    public int trollPerSecond = 0;
+    public int countTrollNow = 0;
+    public int level = 1;
+    public int trollCount = 0;
+
+
 }
--- Clicker/Assets/Scripts/ViewTextUpTheFace.cs
+++ Clicker/Assets/Scripts/ViewTextUpTheFace.cs
@@ -5,11 +5,18 @@ public class ViewTextUpTheFace : MonoBehaviour
 {
     [SerializeField] private TMP_Text _textUpTheFace;
     public TrollCounter _trollCounter;
+    public GameObject _prefab;
+    public Transform _canvas;
+    private GameObject _instantiate;
+    public TMP_Text _count;
 
     public void Click()
     {
+        _textUpTheFace.text = ($"+ {_trollCounter.trollPerClick.ToString()}");
         _trollCounter.countTrollNow += _trollCounter.trollPerClick;
-        _textUpTheFace.text = _trollCounter.trollPerClick.ToString();
+        _count.text = _trollCounter.countTrollNow.ToString();
+        _instantiate = Instantiate(_prefab, _canvas.transform);
+        Destroy(_instantiate, 1);
     }
 
 }
