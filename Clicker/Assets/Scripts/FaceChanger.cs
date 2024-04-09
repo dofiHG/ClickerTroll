a36a164f6ab9e30c04404d196d832ea50d05d2e5
dofiHG <dofihgsafafafa@gmail.com>
2024-04-09 13:05:08 +0300

    Finall1

--- Clicker/Assets/Scripts/FaceChanger.cs
+++ Clicker/Assets/Scripts/FaceChanger.cs
@@ -23,55 +23,55 @@ public class FaceChanger : MonoBehaviour
             case 1: trollFace.sprite = spritesArray[0]; break;
             case 2: 
                 trollFace.sprite = spritesArray[1];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
             case 3: 
                 trollFace.sprite = spritesArray[2];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(456, 405);
                 break;
             case 4: 
                 trollFace.sprite = spritesArray[3];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(456, 405);
                 break;
             case 5: 
                 trollFace.sprite = spritesArray[4];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(456, 405);
                 break;
             case 6: 
                 trollFace.sprite = spritesArray[5];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(456, 405);
                 break;
             case 7: 
                 trollFace.sprite = spritesArray[6];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
             case 8:
                 trollFace.sprite = spritesArray[7];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
             case 9:
                 trollFace.sprite = spritesArray[8];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
             case 10:
                 trollFace.sprite = spritesArray[9];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
             case 11:
                 trollFace.sprite = spritesArray[10];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
             case 12:
                 trollFace.sprite = spritesArray[11];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
             case 13:
                 trollFace.sprite = spritesArray[12];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
             case 14:
                 trollFace.sprite = spritesArray[13];
-                trollFace.rectTransform.sizeDelta = new Vector2(356, 295);
+                trollFace.rectTransform.sizeDelta = new Vector2(506, 455);
                 break;
 
         }
